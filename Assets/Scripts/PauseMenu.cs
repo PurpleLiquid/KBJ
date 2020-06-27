5fba08d4a683836caf2463bf5c3cfe28eccc2a91
Arthurpod <jonillsonbaquiran@gmail.com>
2020-06-27 08:53:16 -0600

    Pause menu and 3rd person movement fix
    Pause using "P". 3rd person horizontal movement fixed by changing camera rotation to Q and E

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool paused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //pause and show pause menu UI
+        if (Input.GetKeyDown(KeyCode.P) & paused == false)
+        {
+            Time.timeScale = 0f;
+            pauseMenuUI.SetActive(true);
+            paused = true;
+        }
+
+        //unpause and hid pause menu UI
+        else if (Input.GetKeyDown(KeyCode.P) & paused == true)
+        {
+            Time.timeScale = 1f;
+            pauseMenuUI.SetActive(false);
+            paused = false;
+
+        }
+    }
+}
