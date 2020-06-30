930d82e9d16607e603adba685b162ef362aa26b0
BlueMeringue <renzbrian.cruz@hotmail.com>
2020-06-29 22:17:57 -0600

    Sample scene where player interacts with goal if in front of it

---
+++ Assets/Scripts/Dummy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// When using unity editor ifs
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class Dummy : MonoBehaviour
+{
+    public bool interactable = false;
+
+    public void goal() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            #if UNITY_EDITOR
+                // Stops editor from playing
+                EditorApplication.isPlaying = false;
+            #else
+                // When not using Unity Editor close application
+                // https://docs.unity3d.com/Manual/PlatformDependentCompilation.html
+                Application.Quit();
+            #endif
+        }
+    }
+}
---
+++ Assets/Scripts/FrontChecker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script is just a sample will change later
+public class FrontChecker : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        layerMask = ~layerMask;
+
+        // Needed to collect what is being hit
+        RaycastHit hit;
+
+        // Draws a straight line. Only for debug purposes
+        // params in order origin, direction, color, duration, depth test
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward), Color.red, 0f, false);
+
+        // Does the ray intersect any objects excluding the player layer
+        // params in order origin, direction, hitinfo, max distance, layer mask
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1f, layerMask))
+        {
+            if (hit.transform.gameObject.GetComponent<Dummy>().interactable)
+            {
+                hit.transform.gameObject.GetComponent<Dummy>().goal();
+            }
+        }
+    }
+}
