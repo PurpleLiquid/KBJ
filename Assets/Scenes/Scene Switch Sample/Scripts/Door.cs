7cc90b57af74a5a545c0492dc6dc4145d0366825
BlueMeringue <renzbrian.cruz@hotmail.com>
2020-07-09 21:08:35 -0600

    Fixing scene switch

--- Assets/Scenes/Scene Switch Sample/Scripts/Door.cs
+++ Assets/Scenes/Scene Switch Sample/Scripts/Door.cs
@@ -7,9 +7,21 @@ public class Door : MonoBehaviour
 {
     public void goNextRoom()
     {
+        // Get current Scene
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
         if (Input.GetKeyDown(KeyCode.Z))
         {
-            SceneManager.LoadScene("Scene Two");
+            if (sceneName == "Scene One")
+            {
+                SceneManager.LoadScene("Scene Two");
+            }
+
+            if(sceneName == "Scene Two")
+            {
+                SceneManager.LoadScene("Scene One");
+            }
         }
     }
 }
---
+++ Assets/Scenes/Scene Switch Sample/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private static Player playerInstance;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        // Stops duplication
+        if (playerInstance == null)
+        {
+            playerInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+}
