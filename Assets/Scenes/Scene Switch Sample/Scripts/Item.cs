f70aab3d375146924f55b2689ed2034432a448a5
BlueMeringue <renzbrian.cruz@hotmail.com>
2020-07-10 22:11:20 -0600

    Preparing items

---
+++ Assets/Scenes/Scene Switch Sample/Scripts/Item.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] string itemName;
+    [SerializeField] Image icon;
+
+    public Item pickUpItem()
+    {
+        return this;
+    }
+
+    public void useItem()
+    {
+        Destroy(this);
+    }
+}
