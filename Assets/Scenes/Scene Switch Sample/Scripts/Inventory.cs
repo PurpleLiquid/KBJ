65368dced6137ade5ef38773ab67816cd2c6381f
BlueMeringue <renzbrian.cruz@hotmail.com>
2020-07-12 22:42:12 -0600

    Inventory access

---
+++ Assets/Scenes/Scene Switch Sample/Scripts/Inventory.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] GameObject inventory;
+    private bool access = false;
+
+    void Start()
+    {
+        // Just in case
+        inventory.SetActive(false);
+    }
+    
+    void Update()
+    {
+        // Accessing Inventory
+        if (Input.GetKeyDown(KeyCode.I) & access == true)
+        {
+            inventory.SetActive(false);
+            access = false;
+        }
+        else if(Input.GetKeyDown(KeyCode.I) & access == false)
+        {
+            inventory.SetActive(true);
+            access = true;
+        }
+    }
+}
