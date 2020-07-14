1cacfae7526c202f23048b5e92f2c10a2ba8d9f2
BlueMeringue <renzbrian.cruz@hotmail.com>
2020-07-13 20:29:53 -0600

    Importing the Nature Starter kit Will organize later

--- Assets/Scenes/Scene Switch Sample/Scripts/Item.cs
+++ Assets/Scenes/Scene Switch Sample/Scripts/Item.cs
@@ -6,9 +6,4 @@ public class Item : MonoBehaviour
 {
     public string itemName = "New Item";
     public Sprite icon = null;
-    
-    public void RemoveFromInventory()
-    {
-        Inventory.instance.Remove(this);
-    }
 }
