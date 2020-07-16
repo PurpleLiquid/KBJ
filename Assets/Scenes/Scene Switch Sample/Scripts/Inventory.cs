bc7a200881757961facb86a75199e775fd4b8c8e
BlueMeringue <renzbrian.cruz@hotmail.com>
2020-07-15 19:49:21 -0600

    Accessing inventory at a press of a button

--- Assets/Scenes/Scene Switch Sample/Scripts/Inventory.cs
+++ Assets/Scenes/Scene Switch Sample/Scripts/Inventory.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     private static Inventory _instance;
+    private List<Item> itemList = new List<Item>();
+    private bool accessed = false;
+
+    // For the visual
+    [SerializeField] InventoryUI ui;
 
     public static Inventory getInstance()
     {
@@ -16,12 +21,58 @@ public class Inventory : MonoBehaviour
     {
         if (_instance != null && _instance != this)
         {
+            // Stops duplication
             Destroy(this.gameObject);
         }
         else
         {
+            // This allows it to switch scenes
             DontDestroyOnLoad(this);
             _instance = this;
         }
+    }
+
+    private void Start()
+    {
+        // Just in case
+        ui.transform.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        // Inputs
+        if (Input.GetKeyDown(KeyCode.I) & accessed == false)
+        {
+            ui.transform.gameObject.SetActive(true);
+            accessed = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.I) & accessed == true)
+        {
+            ui.transform.gameObject.SetActive(false);
+            accessed = false;
+        }
+    }
+
+    public void addItem(Item item)
+    {
+        itemList.Add(item);
+    }
+
+    public Item getItem(string itemName)
+    {
+        Item item = null;
+
+        for(int i = 0; i < itemList.Count; i++)
+        {
+            item = itemList[i];
+
+            if(itemName == item.itemName)
+            {
+                return item;
+            }
+        }
+
+        // Didn't find it
+        return null;
     }
 }
--- Assets/Scenes/Scene Switch Sample/Scripts/InventoryUI.cs
+++ Assets/Scenes/Scene Switch Sample/Scripts/InventoryUI.cs
@@ -1,6 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class InventoryUI : MonoBehaviour
 {
-    
+    private List<ItemSlot> itemSlotList;
+
+    private void Start()
+    {
+        foreach(Transform child in transform)
+        {
+            ItemSlot slot = child.GetComponent<ItemSlot>();
+            itemSlotList.Add(slot);
+        }
+    }
+
+    public void addItemVisually(Item item)
+    {
+
+    }
 }
