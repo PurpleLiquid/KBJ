67ed064712f194bfeb1d64180a9fd917ed76beae
BlueMeringue <renzbrian.cruz@hotmail.com>
2020-07-31 21:55:32 -0600

    Some more organization and have a functional title screen, fixed fades (Just need the play button to go to a scene)

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// When using unity editor ifs
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+/* ==============================
+ * NOTE
+ * This script will constantly be changing as more scenes are added in
+ * I will find a way to make this scalable --Brian
+ */
+public class LevelChanger : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    public void Fade()
+    {
+        animator.SetTrigger("FadeOut");
+    }
+
+    // ================  Title Screen  ========================
+    public void Play()
+    {
+        print("Pressing Play");
+    }
+
+    public void Tutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void Exit()
+    {
+        #if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
