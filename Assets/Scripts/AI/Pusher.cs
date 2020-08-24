bd53116654a8b98bb67bcc6cff1d831d1fada885
BlueMeringue <renzbrian.cruz@hotmail.com>
2020-08-23 19:56:00 -0600

    Updating lock icons, Preparing foyer

---
+++ Assets/Scripts/AI/Pusher.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Pusher : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float visionRange = 5f;
+
+    private NavMeshAgent navMeshAgent;
+    private float distanceToTarget = Mathf.Infinity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (distanceToTarget <= visionRange)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, visionRange);
+    }
+}
