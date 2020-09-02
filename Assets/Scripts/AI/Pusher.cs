3bd56d751e4aed171d064c9122b92252f0c77cc9
BlueMeringue <renzbrian.cruz@hotmail.com>
2020-09-01 20:03:25 -0600

    Pusher satisfactory

--- Assets/Scripts/AI/Pusher.cs
+++ Assets/Scripts/AI/Pusher.cs
@@ -5,17 +5,19 @@ public class Pusher : MonoBehaviour
 {
     [SerializeField] Transform target;
     [SerializeField] float visionRange = 10f;
-    [SerializeField] float aggroRange = 7f;
 
     private NavMeshAgent navMeshAgent;
     private Animator animator;
     private float distanceToTarget = Mathf.Infinity;
+    private Vector3 originPos;
+    private Vector3 targetBeforeChargePos;
 
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+        originPos = transform.position;
     }
 
     // Update is called once per frame
@@ -25,7 +27,35 @@ public class Pusher : MonoBehaviour
 
         if (distanceToTarget <= visionRange)
         {
-            navMeshAgent.SetDestination(target.position);
+            animator.SetBool("InRange", true);
+            animator.SetBool("Idle", false);
+            targetBeforeChargePos = target.position;
+        }
+    }
+
+    public void ChargePlayer()
+    {
+        navMeshAgent.SetDestination(targetBeforeChargePos);
+    }
+
+    public void ReturnToOrigin()
+    {
+        navMeshAgent.SetDestination(originPos);
+
+        if(transform.position.z == originPos.z)
+        {
+            animator.SetBool("Idle", true);
+        }
+    }
+
+    public void CheckVisionAfterCharge()
+    {
+        if (targetBeforeChargePos.z == transform.position.z)
+        {
+            if ((distanceToTarget <= visionRange) == false)
+            {
+                animator.SetBool("InRange", false);
+            }
         }
     }
 
