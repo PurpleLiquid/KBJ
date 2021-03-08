5ff56b94e01a55ebae0bad2e740a757f285eff58
Meringue <renzbrian.cruz@hotmail.com>
2021-03-07 22:48:45 -0700

    Some changes

---
+++ Assets/Scripts/Death Screen/DialogueManagerOfTheDead.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManagerOfTheDead : MonoBehaviour
+{
+    private Queue<string> sentences = new Queue<string>();
+    [SerializeField] Text nameText;
+    [SerializeField] Text dialogueText;
+
+    // The white box thingy
+    [SerializeField] Image dialogueBox;
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        sentences.Clear();
+
+        nameText.text = dialogue.name;
+        dialogueBox.gameObject.SetActive(true);
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        // Outputs the first sentence
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    private void EndDialogue()
+    {
+        dialogueBox.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Mechanics/FallingDeath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FallingDeath : MonoBehaviour
+{
+    private string deathScreen = "Dead End";
+    [SerializeField] LevelChanger fadeUI;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(ShowDeathScreen());
+    }
+
+    private IEnumerator ShowDeathScreen()
+    {
+        fadeUI.setNextScene(deathScreen);
+        fadeUI.Fade();
+
+        yield return new WaitForSeconds(2);
+        fadeUI.PlayNextScene();
+    }
+}
